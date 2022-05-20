9c006e33528fc116ff882daad9339a486263a854
VRGunnar <gunnarvanremoortere@hotmail.com>
2022-05-20 11:32:28 +0200

    Add users table with the updated entities
    Existing migration for users has been edited, all the new data for the users in our testing environment is added.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('enrolled');
+            $table->string('r-nr');
+            $table->string('password');
+            $table->string('personal_nr');
+            $table->string('library_nr');
+            $table->string('school');
+            $table->string('card_nr');
             $table->rememberToken();
             $table->timestamps();
         });
