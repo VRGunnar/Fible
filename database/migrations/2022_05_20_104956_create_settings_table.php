c6decb3d77f4bb12db4102a95f10d35b13b2fbdd
VRGunnar <gunnarvanremoortere@hotmail.com>
2022-05-20 12:53:19 +0200

    Add rest of important tables
    Table discounts, subscriptions and settings have been added. More tables might come once we setup the relationships between the tables.

---
+++ database/migrations/2022_05_20_104956_create_settings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('notifications');
+            $table->boolean('auto_updates');
+            $table->boolean('dark_mode');
+            $table->string('language', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+};
