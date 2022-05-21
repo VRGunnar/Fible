6d2ff841ba47d48e6e3b285173a2a488a4e7dd7d
VRGunnar <gunnarvanremoortere@hotmail.com>
2022-05-21 20:04:11 +0200

    Add the relations between Models: user, discount, subscription
    Made all the many to many relations for user and discount/subscription. Now the discounts and subscriptions per user are available.

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function discounts()
+    {
+        return $this->belongsToMany(Discount::class);
+    }
+
+    public function subscriptions()
+    {
+        return $this->belongsToMany(Subscription::class);
+    }
 }
