ee1f14b1a3002880ac266c6fd08af62a5be55fcb
VRGunnar <gunnarvanremoortere@hotmail.com>
2022-05-21 17:12:56 +0200

    Edit discountController so it shows discounts and subscriptions

--- app/Http/Controllers/DiscountController.php
+++ app/Http/Controllers/DiscountController.php
@@ -7,8 +7,10 @@ use Illuminate\Http\Request;
 class DiscountController extends Controller
 {
     public function index(){
-        $discount = \DB::table('discounts')->get();
-        $data['discount'] = $discount;
+        $discounts = \DB::table('discounts')->get();
+        $subscriptions = \DB::table('subscriptions')->get();
+        $data['discount'] = $discounts;
+        $data['subscription'] = $subscriptions;
         return view('discount/index', $data);
     }
 }
