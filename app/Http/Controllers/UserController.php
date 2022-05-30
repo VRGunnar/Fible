e3dfa9e40362a32a0e281d39c9abac8fd67ca3d6
VRGunnar <gunnarvanremoortere@hotmail.com>
2022-05-30 19:08:18 +0200

    Edit usercontroller and login.blade.php and routes ragarding login, Add layouts/login,
    Added a temporary login system so we can use a login and develop functionalities which are dependant on a logged in user.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,9 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    //
+    public function login()
+    {
+        return view('/login');
+    }
+
+    public function handleLogin(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('/');
+        } else {
+            return view('/login');
+        }
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('/login');
+    }
 }
