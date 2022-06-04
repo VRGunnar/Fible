a2d6794798b8ef230bf11a93de1ed629ee4742cd
JonasDT98 <jonas.del.turco@gmail.com>
2022-06-04 18:52:04 +0200

    Worked on Dashboard
    I am working on the nav for the dashboard page. Svg are in place just
    need to style it further

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,4 +10,12 @@ class DashboardController extends Controller
     public function index(){
         return view('Dashboard/index');
     }
+
+    public function download(){
+        $filename = 'Contract_fible.pdf';
+        $tempfile = tempname(sys_get_temp_dir(), $filename);
+        copy('http://localhost/dashboard/Contract_fible.pdf', $tempfile);
+
+        return response()->download($tempfile, $filename);
+    }
 }
